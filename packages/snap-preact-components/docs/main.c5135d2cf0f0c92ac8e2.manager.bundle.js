31ad5a814ff3d813069d6fe18191b1cad2b551d5
searchspring-machine <machine@searchspring.com>
2021-06-15 17:25:45 +0000

    chore(docs): generation

---
+++ packages/snap-preact-components/docs/main.c5135d2cf0f0c92ac8e2.manager.bundle.js
@@ -0,0 +1,41 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+	[1],
+	{
+		458: function (module, exports, __webpack_require__) {
+			__webpack_require__(459),
+				__webpack_require__(853),
+				__webpack_require__(847),
+				__webpack_require__(854),
+				__webpack_require__(848),
+				__webpack_require__(855),
+				__webpack_require__(850),
+				__webpack_require__(849),
+				__webpack_require__(851),
+				__webpack_require__(852),
+				(module.exports = __webpack_require__(846));
+		},
+		526: function (module, exports) {},
+		846: function (module, exports, __webpack_require__) {
+			'use strict';
+			__webpack_require__(25).addons.setConfig({ refs: {} });
+		},
+		853: function (module, __webpack_exports__, __webpack_require__) {
+			'use strict';
+			__webpack_require__.r(__webpack_exports__);
+			var esm = __webpack_require__(81),
+				create = __webpack_require__(429),
+				searchspring_logo = __webpack_require__.p + 'static/media/searchspring-logo.fb3e0170.svg',
+				searchspringTheme = Object(create.create)({
+					base: 'light',
+					colorPrimary: '#3a23ad',
+					colorSecondary: '#00cee1',
+					appBg: '#fafafa',
+					barSelectedColor: '#3a23ad',
+					brandTitle: 'Searchspring Snap Preact',
+					brandImage: searchspring_logo,
+				});
+			esm.c.setConfig({ theme: searchspringTheme });
+		},
+	},
+	[[458, 2, 3]],
+]);
