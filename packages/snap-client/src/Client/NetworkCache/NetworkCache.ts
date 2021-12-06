de0740dfbd3810eee12b47f16b3d2795ed27a837
Chris <chris.frazier@searchspring.com>
2021-12-06 09:07:44 -0800

    refactor(networkcache): wrapping in a try catch to prevent errors when cookies are disabled

--- packages/snap-client/src/Client/NetworkCache/NetworkCache.ts
+++ packages/snap-client/src/Client/NetworkCache/NetworkCache.ts
@@ -7,50 +7,62 @@ export class NetworkCache<T> {
 	};
 
 	public get(key: number | string | undefined): T | undefined {
-		const stored: any = sessionStorage.getItem('ss-networkcache');
-		const localData = stored && JSON.parse(stored);
-		if (localData && key && localData[key]) {
-			const now = new Date();
-			// compare the expiry time of the item with the current time
-			if (now.getTime() > localData[key].expires) {
-				// remove item
-				const newStored = {
-					...localData,
-				};
-				delete newStored[key];
-				//update storage
-				sessionStorage.setItem('ss-networkcache', JSON.stringify(newStored));
+		try {
+			const stored: any = sessionStorage.getItem('ss-networkcache');
+			const localData = stored && JSON.parse(stored);
+			if (localData && key && localData[key]) {
+				const now = new Date();
+				// compare the expiry time of the item with the current time
+				if (now.getTime() > localData[key].expires) {
+					// remove item
+					const newStored = {
+						...localData,
+					};
+					delete newStored[key];
+					//update storage
+					sessionStorage.setItem('ss-networkcache', JSON.stringify(newStored));
 
-				return undefined;
+					return undefined;
+				} else {
+					return localData[key].value;
+				}
 			} else {
-				return localData[key].value;
+				return undefined;
 			}
-		} else {
-			return undefined;
+		} catch (err) {
+			console.warn('something went wrong, browser might not have cookies enabled');
 		}
 	}
 
 	public set(key: number | string, value: any, cacheSettings?: CacheConfig): void {
-		if (!cacheSettings) {
-			cacheSettings = this.defaultCacheSettings;
-		}
+		try {
+			if (!cacheSettings) {
+				cacheSettings = this.defaultCacheSettings;
+			}
 
-		if (cacheSettings.enabled) {
-			const stored: any = sessionStorage.getItem('ss-networkcache');
-			const newStored = {
-				...(stored && JSON.parse(stored)),
-			};
-			const now = new Date();
-			newStored[key] = {
-				value: JSON.stringify(value),
-				expires: now.getTime() + cacheSettings.expiresAfter,
-			};
+			if (cacheSettings.enabled) {
+				const stored: any = sessionStorage.getItem('ss-networkcache');
+				const newStored = {
+					...(stored && JSON.parse(stored)),
+				};
+				const now = new Date();
+				newStored[key] = {
+					value: JSON.stringify(value),
+					expires: now.getTime() + cacheSettings.expiresAfter,
+				};
 
-			sessionStorage.setItem('ss-networkcache', JSON.stringify(newStored));
+				sessionStorage.setItem('ss-networkcache', JSON.stringify(newStored));
+			}
+		} catch (err) {
+			console.warn('something went wrong, browser might not have cookies enabled');
 		}
 	}
 
 	public clear() {
-		sessionStorage.setItem('ss-networkcache', '');
+		try {
+			sessionStorage.setItem('ss-networkcache', '');
+		} catch (err) {
+			console.warn('something went wrong, browser might not have cookies enabled');
+		}
 	}
 }
