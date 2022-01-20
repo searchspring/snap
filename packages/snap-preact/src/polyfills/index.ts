6a371aac0a417011b3b76d26a723c66a5b84502a
Dennis Konieczek <konieczekdennis@gmail.com>
2022-01-20 13:56:33 -0500

    fix: remove fetch and core-js from polyfills

--- packages/snap-preact/src/polyfills/index.ts
+++ packages/snap-preact/src/polyfills/index.ts
@@ -1,21 +1,11 @@
-import 'core-js/features/promise';
-export function polyfills(): Promise<void> {
-	return new Promise(function (resolve) {
-		// eslint-disable-next-line no-var
-		var promises = [];
-
-		if (!('fetch' in window)) {
-			promises.push(import('whatwg-fetch'));
-		}
+export const polyfills = (): Promise<void> => {
+	return new Promise((resolve) => {
+		const promises = [];
 		if (!('IntersectionObserver' in window)) {
 			promises.push(import('intersection-observer'));
 		}
-		if (!('Symbol' in window) || !('flatMap' in Array.prototype) || !('includes' in Array.prototype)) {
-			promises.push(import('core-js/stable'));
-		}
-
-		Promise.all(promises).then(function () {
+		Promise.all(promises).then(() => {
 			resolve();
 		});
 	});
-}
+};
