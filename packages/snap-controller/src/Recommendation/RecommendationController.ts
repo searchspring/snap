c2c2eab0c82b9cbeda25e5aa24a7da02a876e3cf
Dennis Konieczek <konieczekdennis@gmail.com>
2022-02-01 12:52:09 -0500

    refactor(recommendationinstantiator): refactor options interface

--- packages/snap-controller/src/Recommendation/RecommendationController.ts
+++ packages/snap-controller/src/Recommendation/RecommendationController.ts
@@ -24,6 +24,7 @@ const defaultConfig: RecommendationControllerConfig = {
 	id: 'recommend',
 	tag: '',
 	batched: true,
+	realtime: false,
 	globals: {},
 };
 
--- packages/snap-preact/src/Instantiators/RecommendationInstantiator.tsx
+++ packages/snap-preact/src/Instantiators/RecommendationInstantiator.tsx
@@ -88,10 +88,26 @@ export class RecommendationInstantiator {
 			async (target, injectedElem, elem) => {
 				const globals: any = {};
 
-				const { shopper, shopperId, product, seed, branch, options, realtime, batched } = getContext(
-					['shopperId', 'shopper', 'product', 'seed', 'branch', 'options', 'realtime', 'batched'],
+				const { shopper, shopperId, product, seed, options, realtime } = getContext(
+					['shopperId', 'shopper', 'product', 'seed', 'options'],
 					elem as HTMLScriptElement
 				);
+
+				/*
+					type instantiatorContext = {
+						shopper?: string;
+						shopperId?: string;
+						product?: string;
+						seed?: string;
+						options?: {
+							siteId?: string;
+							branch?: string;
+							batched?: boolean;
+							realtime?: boolean;
+							categories?: any;
+						}
+					}
+				*/
 
 				if (shopper || shopperId) {
 					globals.shopper = shopper || shopperId;
@@ -99,16 +115,13 @@ export class RecommendationInstantiator {
 				if (product || seed) {
 					globals.product = product || seed;
 				}
-				if (branch) {
-					globals.branch = branch;
+				if (options?.branch) {
+					globals.branch = options.branch;
 				}
-				if (typeof batched === 'boolean') {
-					globals.batched = batched;
-				}
-				if (options && options.siteId) {
+				if (options?.siteId) {
 					globals.siteId = options.siteId;
 				}
-				if (options && options.categories) {
+				if (options?.categories) {
 					globals.categories = options.categories;
 				}
 
@@ -118,10 +131,12 @@ export class RecommendationInstantiator {
 				const controllerConfig = {
 					id: `recommend_${tag + (profileCount[tag] - 1)}`,
 					tag,
-					realtime: Boolean(realtime),
+					batched: options?.batched ?? true,
+					realtime: Boolean(options?.realtime),
 					globals,
 					...this.config.config,
 				};
+
 				const createRecommendationController = (await import('../create/createRecommendationController')).default;
 				const client = this.config.services?.client || this.client;
 				const tracker = this.config.services?.tracker || this.tracker;
