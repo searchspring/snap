e5fedfb9f67d8f7e837684c43b19b08051460e48
kevin <kevin@searchspring.com>
2022-04-15 20:37:25 -0600

    test(snapify): adding createController functions to Snapfiy storybook helper

--- packages/snap-preact-components/src/utilities/snapify.ts
+++ packages/snap-preact-components/src/utilities/snapify.ts
@@ -1,14 +1,24 @@
 import { h, render } from 'preact';
-/* searchspring imports */
-import { createSearchController, createAutocompleteController, createRecommendationController } from '@searchspring/snap-preact';
-import type {
-	SearchController,
-	AutocompleteController,
-	RecommendationController,
-	SearchControllerConfig,
-	AutocompleteControllerConfig,
-	RecommendationControllerConfig,
-} from '@searchspring/snap-controller';
+
+import { SearchController, AutocompleteController, RecommendationController } from '@searchspring/snap-controller';
+import { Client } from '@searchspring/snap-client';
+import { SearchStore, AutocompleteStore, RecommendationStore } from '@searchspring/snap-store-mobx';
+import { UrlManager, UrlTranslator, reactLinker } from '@searchspring/snap-url-manager';
+import { EventManager } from '@searchspring/snap-event-manager';
+import { Profiler } from '@searchspring/snap-profiler';
+import { Logger } from '@searchspring/snap-logger';
+import { Tracker } from '@searchspring/snap-tracker';
+
+import type { ClientConfig, ClientGlobals } from '@searchspring/snap-client';
+import type { SearchControllerConfig, AutocompleteControllerConfig, RecommendationControllerConfig } from '@searchspring/snap-controller';
+
+type CreateConfig = {
+	client: {
+		globals?: ClientGlobals;
+		config?: ClientConfig;
+	};
+	controller: SearchControllerConfig | AutocompleteControllerConfig | RecommendationControllerConfig;
+};
 
 const controllers = {};
 const client = {
@@ -70,4 +80,51 @@ export class Snapify {
 
 		return cntrlr;
 	}
+}
+
+function createSearchController(config: CreateConfig): SearchController {
+	const urlManager = new UrlManager(new UrlTranslator(), reactLinker);
+
+	const cntrlr = new SearchController(config.controller as SearchControllerConfig, {
+		client: new Client(config.client.globals, config.client.config),
+		store: new SearchStore(config.controller as SearchControllerConfig, { urlManager }),
+		urlManager,
+		eventManager: new EventManager(),
+		profiler: new Profiler(),
+		logger: new Logger(),
+		tracker: new Tracker(config.client.globals),
+	});
+
+	return cntrlr;
+}
+
+function createRecommendationController(config: CreateConfig): RecommendationController {
+	const urlManager = new UrlManager(new UrlTranslator(), reactLinker).detach(true);
+	const cntrlr = new RecommendationController(config.controller as RecommendationControllerConfig, {
+		client: new Client(config.client.globals, config.client.config),
+		store: new RecommendationStore(config.controller as RecommendationControllerConfig, { urlManager }),
+		urlManager,
+		eventManager: new EventManager(),
+		profiler: new Profiler(),
+		logger: new Logger(),
+		tracker: new Tracker(config.client.globals),
+	});
+
+	return cntrlr;
+}
+
+function createAutocompleteController(config: CreateConfig): AutocompleteController {
+	const urlManager = new UrlManager(new UrlTranslator(), reactLinker).detach();
+
+	const cntrlr = new AutocompleteController(config.controller as AutocompleteControllerConfig, {
+		client: new Client(config.client.globals, config.client.config),
+		store: new AutocompleteStore(config.controller as AutocompleteControllerConfig, { urlManager }),
+		urlManager,
+		eventManager: new EventManager(),
+		profiler: new Profiler(),
+		logger: new Logger(),
+		tracker: new Tracker(config.client.globals),
+	});
+
+	return cntrlr;
 }
