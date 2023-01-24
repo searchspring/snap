d3b96eb788392a35ed5366033a2f175b40c1650e
Chris <chris@Chriss-MacBook-Pro.local>
2023-01-24 11:30:04 -0800

    refactor(usea11y): only add 1 style block

--- packages/snap-preact-components/src/hooks/useA11y.tsx
+++ packages/snap-preact-components/src/hooks/useA11y.tsx
@@ -1,13 +1,14 @@
 import { h } from 'preact';
-import { useEffect } from 'preact/hooks';
 
 export function useA11y(elem: any, tabIndex?: number, focusRingColor?: string) {
-	useEffect(() => {
+	const styleId = 'ssA11yFocusStyle';
+	if (!document.querySelector(`#${styleId}`)) {
 		var style = document.createElement('style');
 		style.type = 'text/css';
+		style.id = styleId;
 		style.innerHTML = `[ssA11y]:focus-visible { outline: ${focusRingColor || '-webkit-focus-ring-color'} auto 1px !important; }`;
 		document.getElementsByTagName('head')[0].appendChild(style);
-	}, []);
+	}
 
 	if (elem && !elem.attributes?.ssA11y) {
 		//A11y attribute is for debouncing on rerenders
