32f84281710ee0239b73158178c8e879ca7410aa
kevin <kevin@searchspring.com>
2023-02-01 18:53:57 -0700

    fix(store-mobx-history): fixing max configuration so that it disables when zero / trims when lowered

--- packages/snap-store-mobx/src/Search/Stores/SearchHistoryStore.ts
+++ packages/snap-store-mobx/src/Search/Stores/SearchHistoryStore.ts
@@ -33,6 +33,20 @@ export class SearchHistoryStore {
 			type: StorageType.LOCAL,
 			key: `ss-history${this.config.siteId ? `-${this.config.siteId}` : ``}`,
 		});
+
+		// reset to zero to clear any potentially existing terms
+		if (this.config.max === 0) {
+			this.reset();
+		}
+
+		// trim history if the current queries are more than config max
+		if (this.queries.length > this.config.max!) {
+			this.getStoredData().forEach((term, index) => {
+				if (index > this.config.max! - 1) {
+					this.remove(term);
+				}
+			});
+		}
 	}
 
 	get queries(): Query[] {
@@ -41,21 +55,23 @@ export class SearchHistoryStore {
 	}
 
 	public save(term: string) {
-		const history = this.getStoredData();
+		// adding term to array if max is not zero
+		if (this.config.max) {
+			const history = this.getStoredData();
 
-		// removing term if already present
-		const index = history.indexOf(term);
-		if (index != -1) {
-			history.splice(index, 1);
+			// removing term if already present
+			const index = history.indexOf(term);
+			if (index != -1) {
+				history.splice(index, 1);
+			}
+
+			history.unshift(term);
+			if (history.length > this.config.max!) {
+				history.pop();
+			}
+
+			this.storage.set('history', JSON.stringify(history));
 		}
-
-		// adding term to array
-		history.unshift(term);
-		if (history.length > this.config.max!) {
-			history.pop();
-		}
-
-		this.storage.set('history', JSON.stringify(history));
 	}
 
 	public remove(term: string) {
