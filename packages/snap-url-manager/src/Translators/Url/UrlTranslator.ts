e93418796bff460ff33f5b6b5144447777a3fa25
chris <chris.frazier@searchspring.com>
2023-03-01 15:13:44 -0800

    chore(url translator): clean up

--- packages/snap-url-manager/src/Translators/Url/UrlTranslator.ts
+++ packages/snap-url-manager/src/Translators/Url/UrlTranslator.ts
@@ -364,7 +364,6 @@ export class UrlTranslator implements Translator {
 	}
 
 	serialize(state: UrlState): string {
-		// debugger
 		const root = this.config.urlRoot.includes('?')
 			? this.config.urlRoot.split('?')[0]
 			: this.config.urlRoot.includes('#')
