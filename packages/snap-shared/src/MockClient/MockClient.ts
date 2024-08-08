6be1acfb969ea2b66237ea86f0c564515bf2ae11
kevin <kevin@searchspring.com>
2024-08-07 21:51:34 -0600

    refactor(mockclient): adding ability to specify a delay to the search in order to test injection

--- packages/snap-shared/src/MockClient/MockClient.ts
+++ packages/snap-shared/src/MockClient/MockClient.ts
@@ -10,21 +10,31 @@ import { MockData } from '../MockData/MockData';
 
 */
 
+export type MockConfig = {
+	delay?: number;
+};
+
 export class MockClient extends Client {
 	mockData: MockData;
+	mockConfig: MockConfig;
 
-	constructor(global: ClientGlobals, config: ClientConfig = {}) {
+	constructor(global: ClientGlobals, config: ClientConfig = {}, mockConfig: MockConfig = {}) {
 		super(global, config);
 
+		this.mockConfig = mockConfig;
 		this.mockData = new MockData({ siteId: global.siteId });
 	}
 
 	async meta() {
+		if (this.mockConfig.delay) await wait(this.mockConfig.delay);
+
 		return this.mockData.meta();
 	}
 
 	async search() {
 		const searchData = this.mockData.search();
+
+		if (this.mockConfig.delay) await wait(this.mockConfig.delay);
 
 		return Promise.all([this.meta() as MetaResponseModel, searchData as SearchResponseModel]);
 	}
@@ -32,20 +42,34 @@ export class MockClient extends Client {
 	async finder() {
 		const searchData = this.mockData.search();
 
+		if (this.mockConfig.delay) await wait(this.mockConfig.delay);
+
 		return Promise.all([this.meta() as MetaResponseModel, searchData as SearchResponseModel]);
 	}
 
 	async autocomplete() {
 		const autocompleteData = this.mockData.autocomplete();
 
+		if (this.mockConfig.delay) await wait(this.mockConfig.delay);
+
 		return Promise.all([this.meta() as MetaResponseModel, autocompleteData as AutocompleteResponseModel]);
 	}
 
 	async recommend() {
+		if (this.mockConfig.delay) await wait(this.mockConfig.delay);
+
 		return this.mockData.recommend();
 	}
 
 	async trending(): Promise<TrendingResponseModel> {
+		if (this.mockConfig.delay) await wait(this.mockConfig.delay);
+
 		return this.mockData.trending();
 	}
+}
+
+function wait(time = 0) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, time);
+	});
 }
