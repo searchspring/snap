050531bbd2b12313380aeb54f6aba6db16adfdca
kevin <kevin@searchspring.com>
2024-09-25 14:42:39 -0600

    docs(preact/storybook): added provider that is observing so that theme changes are picked up

--- packages/snap-preact/components/.storybook/preview.tsx
+++ packages/snap-preact/components/.storybook/preview.tsx
@@ -1,20 +1,68 @@
-import { h } from 'preact';
+import { ComponentChildren, Fragment, h } from 'preact';
 import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { observer } from 'mobx-react-lite';
 
+import { SnapTemplates, TemplatesStore } from '../../src';
 import { ThemeProvider } from '../src/providers/theme';
 import { bocachica } from '../src/themes';
 
 // custom styles for storybook
 import './styles.scss';
+import { SnapProvider, Theme } from '../src';
+
+// snap instance for theming and templates functionality
+const snapTemplates = new SnapTemplates({
+	config: {
+		siteId: '8uyt2m',
+	},
+	themes: {
+		global: {
+			extends: 'bocachica',
+		},
+	},
+});
+
+// need to add each theme synchronously
+
+// bocachica
+snapTemplates.templates.addTheme({
+	name: 'bocachica',
+	type: 'library',
+	base: bocachica,
+	language: {},
+	languageOverrides: {},
+	currency: {},
+	innerWidth: window.innerWidth,
+});
+
+const Providers = observer(
+	({ templateStore, children, themeName }: { templateStore: TemplatesStore; themeName: string; children: ComponentChildren }) => {
+		const themeLocation = templateStore.themes.library[themeName];
+		const mergedTheme = themeLocation?.theme || {};
+		return (
+			<SnapProvider snap={snapTemplates}>
+				<ThemeProvider theme={mergedTheme}>{children}</ThemeProvider>
+			</SnapProvider>
+		);
+	}
+);
+
+const CustomThemeProvider = ({ theme, children }: { theme: Theme; children: ComponentChildren }) => {
+	return (
+		<Providers templateStore={snapTemplates.templates} themeName={theme.name!}>
+			{children}
+		</Providers>
+	);
+};
 
 export const decorators = [
 	withThemeFromJSXProvider({
 		themes: {
-			bocachica: bocachica,
+			bocachica: () => snapTemplates.templates.themes.library.bocachica.theme,
 			none: {},
 		},
-		defaultTheme: 'bocachica',
-		Provider: ThemeProvider,
+		defaultTheme: 'none',
+		Provider: CustomThemeProvider,
 	}),
 ];
 
