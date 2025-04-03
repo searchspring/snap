b58a876a9da4fc309df45fd401a10f051ad86243
Chris <chris.frazier@searchspring.com>
2025-04-03 11:17:27 -0700

    chore(preact/components/autocompletetemplate): type fix for build error

--- packages/snap-preact/components/src/components/Templates/AutocompleteTemplate/AutocompleteTemplate.tsx
+++ packages/snap-preact/components/src/components/Templates/AutocompleteTemplate/AutocompleteTemplate.tsx
@@ -563,7 +563,7 @@ export const AutocompleteTemplate = observer((properties: AutocompleteTemplatePr
 	function hasElemsToShow(element: Element) {
 		if (!element.children.length) return true;
 
-		for (const child of element.children) {
+		for (const child of element.children as any) {
 			if (child.tagName !== 'DIV' || child.innerHTML.trim() !== '') {
 				return true;
 			}
