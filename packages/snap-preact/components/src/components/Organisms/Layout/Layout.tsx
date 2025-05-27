6ef8f69d1a3803c0720d2ad4e80d62d7a3b30a54
Chris <chris.frazier@searchspring.com>
2025-05-27 14:36:22 -0700

    fix(preact/components): some bug fixes around ac no results component and layout components both causing rerenders and jankyness

--- packages/snap-preact/components/src/components/Organisms/Layout/Layout.tsx
+++ packages/snap-preact/components/src/components/Organisms/Layout/Layout.tsx
@@ -33,6 +33,9 @@ const defaultStyles: StyleScript<LayoutProps> = ({}) => {
 		'.ss__layout__separator': {
 			flex: '1 1 auto',
 		},
+		'.ss__layout__row:empty': {
+			display: 'none',
+		},
 		'.ss__layout__row': {
 			display: 'flex',
 			alignItems: 'center',
