b6d454bbde4fff68378662ac4a67af6243fcf2fb
kevin <kevin@searchspring.com>
2025-06-30 12:20:16 -0600

    fix(client/cache): updating logic for array response of recommendation API for _cached flag

--- packages/snap-client/src/Client/Client.ts
+++ packages/snap-client/src/Client/Client.ts
@@ -203,6 +203,8 @@ export class Client {
 			...profile,
 			meta,
 			results: recommendations[0] && recommendations[0].results,
+			// @ts-ignore - temporary to be removed when auto beaconing is implemented
+			_cached: !!profile._cached && !!recommendations[0]._cached,
 		};
 	}
 }
