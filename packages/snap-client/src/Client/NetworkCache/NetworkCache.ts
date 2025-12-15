0534b6c4ccb62389669c19f8f9f87cbd098fa6a5
Chris <chris.frazier@searchspring.com>
2025-12-15 15:00:29 -0800

    fix(networkcache): bugfix to prevent memoryCache from growing too large, and delete expired entries

--- packages/snap-client/src/Client/NetworkCache/NetworkCache.ts
+++ packages/snap-client/src/Client/NetworkCache/NetworkCache.ts
@@ -18,6 +18,9 @@ export class NetworkCache {
 	constructor(config?: CacheConfig) {
 		this.config = deepmerge(defaultConfig, config || {});
 
+		this.load();
+
+		// this allows you to pre-populate the cache from the config - primarily used for email recs
 		this.config?.entries &&
 			Object.keys(this.config.entries).map((key: string) => {
 				if (this.config.entries && this.config.entries[key]) {
@@ -26,90 +29,118 @@ export class NetworkCache {
 			});
 	}
 
+	public load(): void {
+		// initialize cache from session storage
+		if (typeof window !== 'undefined' && window?.sessionStorage) {
+			const stored: any = window.sessionStorage.getItem(CACHE_STORAGE_KEY);
+			const newStored: Cache = {
+				...(stored && JSON.parse(stored)),
+			};
+
+			this.memoryCache = newStored || {};
+		}
+
+		this.purgeExpired();
+	}
+
 	public get(key: string): Response | void {
 		if (this.config.enabled) {
+			this.load();
+
 			try {
-				if (this.memoryCache[key]) {
-					if (Date.now() < this.memoryCache[key].expires) {
-						return deepmerge({}, this.memoryCache[key].value);
-					}
-				}
-
 				let ignoreKeys: string[] = [];
-				const navigationType = (performance.getEntriesByType('navigation')[0] as PerformanceNavigationTiming | undefined)?.type;
-				if (typeof window !== 'undefined' && navigationType === 'back_forward') {
-					ignoreKeys = ['lastViewed', 'cart'];
+				if (typeof window !== 'undefined') {
+					const navigationType = (window.performance?.getEntriesByType('navigation')?.[0] as PerformanceNavigationTiming | undefined)?.type;
+					if (navigationType === 'back_forward') {
+						ignoreKeys = ['lastViewed', 'cart'];
+					}
 				}
 
-				if (typeof window !== 'undefined' && window?.sessionStorage) {
-					const stored = window.sessionStorage.getItem(CACHE_STORAGE_KEY);
-					const localData: Cache = stored && JSON.parse(stored);
+				if (Object.keys(this.memoryCache).length && key) {
+					let storageKey = key;
 
-					if (localData && key) {
-						let storageKey = key;
+					//this only applies to search calls
+					if (ignoreKeys.length && key.startsWith('/api/search/search.json')) {
+						try {
+							const url = key.split('{')[0];
+							const payload = '{' + key.split('{')[1];
+							const searchParams = JSON.parse(payload);
 
-						//this only applies to search calls
-						if (ignoreKeys.length && key.startsWith('/api/search/search.json')) {
-							try {
-								const url = key.split('{')[0];
-								const payload = '{' + key.split('{')[1];
-								const searchParams = JSON.parse(payload);
+							const foundKey = Object.keys(this.memoryCache).find((cachedResponse) => {
+								try {
+									const storedUrl = cachedResponse.split('{')[0];
+									if (storedUrl == url) {
+										const storedPayload = '{' + cachedResponse.split('{')[1];
 
-								const foundKey = Object.keys(localData).find((cachedResponse) => {
-									try {
-										const storedUrl = cachedResponse.split('{')[0];
-										if (storedUrl == url) {
-											const storedPayload = '{' + cachedResponse.split('{')[1];
+										const parsedPayload = JSON.parse(storedPayload);
 
-											const parsedPayload = JSON.parse(storedPayload);
+										const allKeys = Array.from(new Set([...Object.keys(searchParams), ...Object.keys(parsedPayload)]));
 
-											const allKeys = Array.from(new Set([...Object.keys(searchParams), ...Object.keys(parsedPayload)]));
-
-											for (const k of allKeys) {
-												if (ignoreKeys.includes(k)) continue;
-												if (JSON.stringify(searchParams[k]) !== JSON.stringify(parsedPayload[k])) {
-													return false;
-												}
+										for (const k of allKeys) {
+											if (ignoreKeys.includes(k)) continue;
+											if (JSON.stringify(searchParams[k]) !== JSON.stringify(parsedPayload[k])) {
+												return false;
 											}
-											return true;
-										} else return false;
-									} catch {
-										return false;
-									}
-								});
-
-								if (foundKey) {
-									storageKey = foundKey;
+										}
+										return true;
+									} else return false;
+								} catch {
+									return false;
 								}
-							} catch (e) {
-								// key is not json
-							}
-						}
+							});
 
-						if (localData[storageKey]) {
-							// compare the expiry time of the item with the current time
-							if (Date.now() >= localData[storageKey].expires) {
-								// remove item
-								const newStored = {
-									...localData,
-								};
-								delete newStored[storageKey];
-								// update storage
-								window.sessionStorage.setItem(CACHE_STORAGE_KEY, JSON.stringify(newStored));
-							} else {
-								return localData[storageKey].value;
+							if (foundKey) {
+								storageKey = foundKey;
 							}
+						} catch (e) {
+							// key is not json
+						}
+					}
+
+					if (this.memoryCache[storageKey]) {
+						// compare the expiry time of the item with the current time
+						if (Date.now() >= this.memoryCache[storageKey].expires) {
+							// remove item
+							const newStored = {
+								...this.memoryCache,
+							};
+							delete newStored[storageKey];
+							// update storage
+							window.sessionStorage.setItem(CACHE_STORAGE_KEY, JSON.stringify(newStored));
+						} else {
+							return this.memoryCache[storageKey].value;
 						}
 					}
 				}
-			} catch {
-				console.warn('something went wrong getting from cache');
+			} catch (err) {
+				console.warn('something went wrong getting from cache', err);
 			}
+		}
+	}
+
+	private purgeExpired(): void {
+		Object.keys(this.memoryCache).forEach((cacheKey) => {
+			//clean up expired cache keys in memory
+			if (Date.now() > this.memoryCache[cacheKey].expires) {
+				delete this.memoryCache[cacheKey];
+			}
+		});
+
+		// update storage
+		try {
+			if (typeof window !== 'undefined' && window?.sessionStorage) {
+				const stringifiedCache = JSON.stringify(this.memoryCache);
+				window.sessionStorage.setItem(CACHE_STORAGE_KEY, stringifiedCache);
+			}
+		} catch {
+			console.warn('failed to store network cache');
 		}
 	}
 
 	public set(key: string, value: Response): void {
 		if (this.config.enabled) {
+			this.load();
+
 			try {
 				const cacheObject = {
 					value,
@@ -117,33 +148,27 @@ export class NetworkCache {
 					purgeable: this.config.purgeable,
 				};
 
+				// purge old items if we are over max size
+				let size = new Blob([JSON.stringify(this.memoryCache)], { endings: 'native' }).size / 1024;
+				while (size > this.config.maxSize) {
+					const oldestKey = Object.keys(this.memoryCache)
+						.filter((key) => this.memoryCache[key].purgeable)
+						.sort((a, b) => {
+							return this.memoryCache[a].expires - this.memoryCache[b].expires;
+						})[0];
+
+					if (!oldestKey) break;
+					delete this.memoryCache[oldestKey];
+
+					// recalculate size after removing oldest
+					size = new Blob([JSON.stringify(this.memoryCache)], { endings: 'native' }).size / 1024;
+				}
+
+				// store cache in memory
 				this.memoryCache[key] = cacheObject;
 
 				if (typeof window !== 'undefined' && window?.sessionStorage) {
-					const stored: any = window.sessionStorage.getItem(CACHE_STORAGE_KEY);
-					const newStored: Cache = {
-						...(stored && JSON.parse(stored)),
-					};
-					newStored[key] = cacheObject;
-
-					let size = new Blob([JSON.stringify(newStored)], { endings: 'native' }).size / 1024;
-					while (size > this.config.maxSize) {
-						const oldestKey = Object.keys(newStored)
-							.filter((key) => newStored[key].purgeable)
-							.sort((a, b) => {
-								return newStored[a].expires - newStored[b].expires;
-							})[0];
-
-						if (!oldestKey) break;
-						delete newStored[oldestKey];
-
-						// recalculate size after removing oldest
-						size = new Blob([JSON.stringify(newStored)], { endings: 'native' }).size / 1024;
-					}
-
-					if (size < this.config.maxSize) {
-						window.sessionStorage.setItem(CACHE_STORAGE_KEY, JSON.stringify(newStored));
-					}
+					window.sessionStorage.setItem(CACHE_STORAGE_KEY, JSON.stringify(this.memoryCache));
 				}
 			} catch {
 				console.warn('something went wrong setting to cache');
