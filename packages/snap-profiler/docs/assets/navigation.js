b3830223405ece090fd07d4983b19fae0f1cb7bf
searchspring-machine <machine@searchspring.com>
2026-03-11 13:50:27 +0000

    chore(docs): generation

--- packages/snap-profiler/docs/assets/navigation.js
+++ packages/snap-profiler/docs/assets/navigation.js
@@ -1,2 +1,1 @@
-window.navigationData =
-	'data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslIKKMpPy8xJLVLSUSpILMlQslJKzkksLk4t1ofJ6GWU5OYo6ShlZ+alKFkZGlnUxgIA5I2Hvz8AAAA=';
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslIKKMpPy8xJLVLSUSpILMlQslJKzkksLk4t1ofJ6GWU5OYo6ShlZ+alKFkZGlnUxgIA5I2Hvz8AAAA="
